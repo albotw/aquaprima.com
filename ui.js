ae5184369c5f759e437910c745c5c8d5b6def3f7
albotw <yann.trou@gmail.com>
2021-03-26 17:25:46 +0100

    patchs
    ajout du js pour l'affichage du menu
    correction largeur des éléments du formulaire

--- ui.js
+++ ui.js
@@ -2,24 +2,8 @@ $(document).ready(function ()
 {
     $(".pure-menu-link").on("click", function ()
     {
-        if (this.hash !== "")
-        {
-            event.preventDefault();
-            let hash = this.hash;
-            let anchor = $(hash);
-            console.log(anchor);
-            console.log(hash);
-            if (anchor != null)
-            {
-                $('html, body').animate({
-                    scrollTop: anchor.offset().top
-                }, 800, function ()
-                {
-                    window.location.hash = hash;
-                });
-                $(this).parent().addClass("pure-menu-selected");
-                $(".pure-menu-link[href ='" + hash + "']").parent().removeClass("pure-menu-selected");
-            }
-        }
+        $(".pure-menu-selected").removeClass("pure-menu-selected");
+        $(this).addClass("pure-menu-selected");
+        $(this.hash)[0].scrollIntoView({ behavior: "smooth", block: "start" });
     });
 });
